6211b2f94a51ee70a2d6d31c600e8afe0f10e852
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-10 15:40:22 -0700

    Added meta char, headers, paragraph, list and table.

--- index.php
+++ index.php
@@ -7,5 +7,49 @@
 		<!-- the title tag will also affect the SEO -->
 		<title>HTML SANDBOX</title>
 	</head>
-	<body></body>
+	<body>
+		<header>
+			<h1>This is a Demo Page!</h1>
+		</header>
+		<main>
+
+			<p>Ristretto java blue mountain extraction breve mazagran, breve macchiato aroma acerbic con panna single shot. Cappuccino robust café au lait aromatic sugar turkish flavour. Percolator in ut et decaffeinated froth as cultivar aroma. Caramelization aroma, extra organic mazagran qui, latte, lungo iced foam qui caffeine.</p>
+			<h2>A Few of My Favorite Things</h2>
+
+			<ul>
+				<li>Cats</li>
+				<li>Water</li>
+				<li>Art</li>
+			</ul>
+			<h3>This Is A Table</h3>
+			<!-- Tables are only used for tabular data -->
+			<table>
+				<!-- Row 1 -->
+				<tr>
+					<th>Column 1</th>
+					<th>Column 2</th>
+				</tr>
+
+				<!-- Row 2 -->
+				<tr>
+					<td>Tabby</td>
+					<td>Beagle</td>
+				</tr>
+
+				<!-- Row 3 -->
+				<tr>
+					<td>Persian</td>
+					<td>Bulldog</td>
+				</tr>
+
+				<!-- Row 4 -->
+				<tr>
+					<td>Siamese</td>
+					<td>Boxer</td>
+				</tr>
+			</table>
+
+
+		</main>
+	</body>
 </html>
