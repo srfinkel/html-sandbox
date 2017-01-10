4334320f963674a3f5fc803a98c80fc41d260df4
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-10 13:43:33 -0700

    Set up bare bones html structure.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- utf-8 is specific to the US language -->
+		<!-- links to JavaScript files, etc -->
+		<meta charset="utf-8" />
+		<!-- the title tag will also affect the SEO -->
+		<title>HTML SANDBOX</title>
+	</head>
+	<body></body>
+</html>
